412e036bd44e25891f0d394be2afc419be17235f
Pavel <paskan59@gmail.com>
2024-04-04 11:22:31 +0300

    Initial commit

---
+++ src/main/java/ca/javau9/thyme/testmaker/TestMakerApplication.java
@@ -0,0 +1,13 @@
+package ca.javau9.thyme.testmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMakerApplication.class, args);
+    }
+
+}
